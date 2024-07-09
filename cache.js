7e32e623aa5a86e9aee844806b463dd70d8707b3
sejoeg <sejoeg@googlemail.com>
2024-07-09 14:43:09 +0200

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,47 +1,46 @@
-const CACHE_NAME = 'restaurant-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
+const CACHE_NAME = 'offline-cache';
+const OFFLINE_URL = '/index.html';
+
+const CACHE_FILES = [
+    '/',
+    '/index.html',
+    // Füge hier andere Ressourcen hinzu, die du zwischenspeichern möchtest
 ];
 
-// Installationsereignis
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Abrufereignis
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache-Hit - Antwort von Cache
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Aktivierungsereignis
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(CACHE_FILES);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request).then(function(response) {
+                let responseClone = response.clone();
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            });
+        }).catch(function() {
+            return caches.match(OFFLINE_URL);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== CACHE_NAME;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
