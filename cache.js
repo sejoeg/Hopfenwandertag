054a25aa133ff45d4cc04761a14a96ebec3cce11
sejoeg <sejoeg@googlemail.com>
2026-06-09 23:38:30 +0200

    update version number to 0.46

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 const GHPATH = "/Hopfenwandertag";
 const APP_PREFIX = "hwt_";
-const VERSION = "version_045";
+const VERSION = "version_046";
 const CACHE_NAME = `${APP_PREFIX}${VERSION}`;
 const URLS = [
   `${GHPATH}/`,
