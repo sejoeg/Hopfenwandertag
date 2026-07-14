0bb5340853ff4e529d26d23b12b5371d723291d2
sejoeg <sejoeg@googlemail.com>
2026-07-14 19:40:44 +0200

    Add configurable app variants

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 const GHPATH = "/Hopfenwandertag";
 const APP_PREFIX = "hwt_";
-const VERSION = "version_047";
+const VERSION = "version_048";
 const CACHE_NAME = `${APP_PREFIX}${VERSION}`;
 const URLS = [
   `${GHPATH}/`,
