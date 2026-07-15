abfd1f0aff944a875cacb34f392fe5b82f8ab678
sejoeg <sejoeg@googlemail.com>
2026-07-15 17:49:36 +0200

    Update cache version to 055 and update food item prices in index.html

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 const GHPATH = "/Hopfenwandertag";
 const APP_PREFIX = "hwt_";
-const VERSION = "version_054";
+const VERSION = "version_055";
 const CACHE_NAME = `${APP_PREFIX}${VERSION}`;
 const URLS = [
   `${GHPATH}/`,
