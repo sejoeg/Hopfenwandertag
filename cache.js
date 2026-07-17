8aeff0623586ea93fb2db9435978c44f220bfa5d
sejoeg <sejoeg@googlemail.com>
2026-07-17 22:28:57 +0200

    Update

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 const GHPATH = "/Hopfenwandertag";
 const APP_PREFIX = "hwt_";
-const VERSION = "version_066";
+const VERSION = "version_067";
 const CACHE_NAME = `${APP_PREFIX}${VERSION}`;
 const URLS = [
   `${GHPATH}/`,
